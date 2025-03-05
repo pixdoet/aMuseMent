7721bc33042f964ab0ac347cd9fc4c30eef70889
Ian Hiew <pixdo.et@gmail.com>
2025-03-05 22:03:35 +0800

    pyinstall

--- config.py
+++ config.py
@@ -4,10 +4,23 @@
 
 import json
 import os
+import sys
+from pathlib import Path
+
+
+def resource_path(relative_path):
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 
 def load_config():
-    with open("config.json", "r") as configFile:
+    with open(resource_path("./config.json"), "r") as configFile:
         data = json.load(configFile)
 
     return data
@@ -15,5 +28,6 @@ def load_config():
 
 # has to be here, cannot put into JSON
 # when used must have leading slash!
-fastFilePath = load_config()["export_folder"]
-DEFAULT_SAVES_PATH = os.path.abspath(fastFilePath)
+# + new: add to user's home folder by default (can be and should be turned off asap)
+fastFilePath = f"{Path.home()}/{load_config()['export_folder']}"
+DEFAULT_SAVES_PATH = resource_path(os.path.abspath(fastFilePath))
